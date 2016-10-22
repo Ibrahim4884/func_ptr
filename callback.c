1745413e64718f2fd8c88c482ecaf1a58ae34c4c
ibrahim <ibrahim@gmail.com>
2016-10-22 14:38:15 +0530

    added line no the print

--- callback.c
+++ callback.c
@@ -5,7 +5,7 @@
 /* callback function definition goes here */
 void my_callback(void)
 {
-    printf("inside my_callback\n");
+    printf(" < %d >inside my_callback\n", __LINE__ );
 }
  
 int main(void)
